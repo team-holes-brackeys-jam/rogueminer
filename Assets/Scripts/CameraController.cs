d210db990ad20b35db533395795fe0bb7c7285c1
vikepic <victorenriquealcazar@gmail.com>
2020-02-17 18:58:14 +0100

    :bookmark: First build!

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,14 +1,20 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public class CameraController : MonoBehaviour
 {
     private Transform _playerTransform;
 
     [SerializeField] private float speed = 3;
+    private Bloom bloomFX;
+    [SerializeField] private float bloomPulseSpeed = 10f;
+
     // Start is called before the first frame update
     private void Start()
     {
+        bloomFX = Camera.main.GetComponent<PostProcessVolume>().profile.GetSetting<Bloom>();
         _playerTransform = PlayerController.Instance.transform;
     }
 
@@ -29,5 +35,23 @@ public class CameraController : MonoBehaviour
         }
         // TODO Better camera pls
         transform.position = Vector3.MoveTowards(transform.position, _playerTransform.position, speed * Time.deltaTime) + Vector3.forward;
+    }
+    
+    IEnumerator PulseBloom()
+    {
+        bloomFX.intensity.value = 30f;
+        while (bloomFX.intensity.value > 10f)
+        {
+            Debug.Log(bloomFX.intensity.value);
+            yield return null;
+            bloomFX.intensity.value -= Time.deltaTime * bloomPulseSpeed;
+        }
+        bloomFX.intensity.value = 10f;
+    }
+
+    public void DoPulseBloom()
+    {
+        StopAllCoroutines();
+        StartCoroutine(nameof(PulseBloom));
     }
 }
--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -1,17 +1,19 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class EnemyController : EntityController
 {
     [SerializeField] private int health = 4;
     [SerializeField] private GameObject coinPrefab;
     [SerializeField] private int coinReward = 2;
-    
+
     public void Hurt()
     {
+        
         --health;
+        Camera.main.GetComponent<CameraController>().DoPulseBloom();
+        
         if (health > 0) return;
 
         var rigidbody2DList = new List<Rigidbody2D>();
@@ -21,11 +23,13 @@ public class EnemyController : EntityController
             rigidbody2DList.Add(coin.GetComponent<Rigidbody2D>());
         }
 
-        foreach (var rigidbody2D in rigidbody2DList)
+        foreach (var rb in rigidbody2DList)
         {
-            rigidbody2D.AddForce(Random.rotation * Vector2.one, ForceMode2D.Impulse);
+            rb.AddForce(Random.rotation * Vector2.one, ForceMode2D.Impulse);
         }
         
         Destroy(gameObject);
     }
+
+
 }
