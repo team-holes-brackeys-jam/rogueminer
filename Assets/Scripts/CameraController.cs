47c6e9a3ccf235c8f2d699a5d00b6a2a57dd257c
vikepic <victorenriquealcazar@gmail.com>
2020-02-21 20:42:19 +0100

    Cleaned up and added ScreenShake

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,36 +1,21 @@
-﻿using System;
-using System.Collections;
+﻿using DG.Tweening;
 using UnityEngine;
-using UnityEngine.Rendering.PostProcessing;
 
 public class CameraController : MonoBehaviour
 {
-    private Transform _playerTransform;
+    private Vector3 originalPos;
 
-    [SerializeField] private float speed = 3;
-    private Bloom bloomFx;
-    [SerializeField] private float bloomPulseSpeed = 10f;
-
+    [SerializeField] private float shakeDuration = 1f;
+    [SerializeField] private float shakeIntensity = 1f;
+    
     private void Start()
     {
-        bloomFx = GetComponent<PostProcessVolume>().profile.GetSetting<Bloom>();
-        _playerTransform = PlayerController.Instance.transform;
+        originalPos = transform.position;
     }
 
-    IEnumerator PulseBloom()
+    public void DoScreenShake()
     {
-        bloomFx.intensity.value = 30f;
-        while (bloomFx.intensity.value > 10f)
-        {
-            yield return null;
-            bloomFx.intensity.value -= Time.deltaTime * bloomPulseSpeed;
-        }
-        bloomFx.intensity.value = 10f;
-    }
-
-    public void DoPulseBloom()
-    {
-        StopAllCoroutines();
-        StartCoroutine(nameof(PulseBloom));
+        transform.position = originalPos;
+        transform.DOShakePosition(shakeDuration, Vector3.one * shakeIntensity);
     }
 }
