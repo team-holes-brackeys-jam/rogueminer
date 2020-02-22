e7501ca2fdd9c6b1105da3139f3961ec0b2092df
vikepic <victorenriquealcazar@gmail.com>
2020-02-22 18:52:43 +0100

    Build 4 :D

---
+++ Assets/NextSlideController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextSlideController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] slides;
+
+    private int currentSlide = 0;
+
+    public void NextSlide()
+    {
+        slides[currentSlide].SetActive(false);
+        ++currentSlide;
+        if (currentSlide >= slides.Length)
+        {
+            if (SceneManager.GetActiveScene().buildIndex + 1 >= SceneManager.sceneCountInBuildSettings)
+            {
+                SceneManager.LoadScene(0);
+            }
+            else
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        }
+        slides[currentSlide].SetActive(true);
+
+    }
+}
