53c8d957c3a569dd663a09184e151bc19421d1a3
vikepic <victorenriquealcazar@gmail.com>
2020-02-22 01:02:18 +0100

    Finished for 2day

---
+++ Assets/Scripts/ArrowController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowController : MonoBehaviour
+{
+    [SerializeField] private float speed = 6f;
+    void Update()
+    {
+        var hit = Physics2D.Raycast(transform.position, transform.up, .1f);
+        if (hit.collider != null)
+        {
+            PlayerController.Instance.canMove = true;
+            Destroy(hit.collider.gameObject);
+            Destroy(gameObject);
+            
+        }
+        transform.position += transform.up * (speed * Time.deltaTime);
+    }
+}
--- Assets/Scripts/TurretController.cs
+++ Assets/Scripts/TurretController.cs
@@ -1,9 +1,10 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class TurretController : MonoBehaviour
 {
     [SerializeField] private float speed = 30f;
+    [SerializeField] private GameObject arrowPrefab;
+    [SerializeField] private GameObject breakFX;
     private void Awake()
     {
         Physics2D.queriesStartInColliders = false;
@@ -11,33 +12,15 @@ public class TurretController : MonoBehaviour
     void Update()
     {
         var hit = Physics2D.Raycast(transform.position, transform.rotation * Vector2.up);
-        if (!hit.collider.CompareTag("Player") && !hit.collider.CompareTag("Turret")) return;
+        if (!hit.collider.CompareTag("Player") && !hit.collider.CompareTag("Turret") ||
+            hit.collider.gameObject == gameObject)
+        {
+            return;
+        }
         PlayerController.Instance.canMove = false;
-        StartCoroutine(nameof(Shoot), hit.collider.gameObject);
-    }
-
-    private IEnumerator Shoot(GameObject value)
-    {
-        if (value == gameObject)
-        {
-            // TODO dirty fix for a very weird bug :thinking:
-            PlayerController.Instance.canMove = true;
-            yield break;
-        }
-        
-        while (Vector3.Distance(transform.position, value.transform.position) > .25f)
-        {
-
-            transform.position = Vector3.MoveTowards(
-                transform.position,
-                value.transform.position,
-                speed * Time.deltaTime);
-            yield return null;
-        }
-        Destroy(value);
+        Instantiate(arrowPrefab, transform.position, transform.rotation);
+        Instantiate(breakFX, transform.position, Quaternion.identity);
         Destroy(gameObject);
-
-        PlayerController.Instance.canMove = true;
     }
 
     public RaycastHit2D CheckCollisions(Vector2 direction)
